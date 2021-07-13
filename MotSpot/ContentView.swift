8c7a7d33f670def7782b636a5749b327aec6ac4a
SIARHEI DUBKO <dolph-1@yandex.ru>
2021-07-13 13:35:08 +0300

    Initial Commit

---
+++ MotSpot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MotSpot
+//
+//  Created by Siarhei Dubko on 13.07.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
